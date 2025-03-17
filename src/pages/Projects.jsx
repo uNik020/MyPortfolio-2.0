ea78f4da6f2a30953c06d8d771a7b20926408d49
uNik020 <shekharnikhil020@gmail.com>
2025-03-17 19:17:59 +0530

    projects almost done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,28 +4,22 @@ import InfiniteMenu from "../blocks/Components/InfiniteMenu/InfiniteMenu.jsx";
 const Projects = () => {
   const items = [
     {
-      image: "https://picsum.photos/300/300?grayscale",
-      link: "https://google.com/",
-      title: "Item 1",
-      description: "This is pretty cool, right?",
+      image: "https://i.imgur.com/Dx011sG.png",
+      link: "https://github.com/uNik020/EstateEase",
+      title: "Estate-Ease",
+      description: "Real estate property management website",
     },
     {
-      image: "https://picsum.photos/400/400?grayscale",
-      link: "https://google.com/",
-      title: "Item 2",
-      description: "This is pretty cool, right?",
+      image: "https://i.imgur.com/dui3Nu2.jpeg",
+      link: "https://github.com/uNik020/MyPortfolio-2.0",
+      title: "My Portfolio",
+      description: "Personal Portfolio website",
     },
     {
-      image: "https://picsum.photos/500/500?grayscale",
-      link: "https://google.com/",
-      title: "Item 3",
-      description: "This is pretty cool, right?",
-    },
-    {
-      image: "https://picsum.photos/600/600?grayscale",
-      link: "https://google.com/",
-      title: "Item 4",
-      description: "This is pretty cool, right?",
+      image: "https://i.imgur.com/uTVVKkR.png",
+      link: "https://stocker-simple-portfolio-tracker.netlify.app/",
+      title: "Stocker",
+      description: "The stock portfolio tracker website.",
     },
   ];
 
@@ -33,7 +27,7 @@ const Projects = () => {
     <>
       <div className="relative flex flex-col items-center">
         <h2 className="absolute z-10 top-4 left-1/2 transform -translate-x-1/2 text-white hover:text-amber-300 orbitron text-3xl sm:text-4xl">
-          Projects
+          My Projects
         </h2>
         <div className="h-screen w-full flex justify-center items-center">
           <InfiniteMenu 
